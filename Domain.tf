b0412e75dca3b3d0fa47e32a310ee7f750cdb3eb
NacteInd <128390991+NacteInd@users.noreply.github.com>
2023-03-27 12:18:14 -0400

    Add files via upload

---
+++ Domain.tf
@@ -0,0 +1,9 @@
+resource "oci_identity_domain" "test_domain" {
+    compartment_id = "ocid1.compartment.oc1..aaaaaaaatsew3msijirazpzqlgfnxhk6ozgv4zngo7zqjgi4uqbeualbfltq"
+    description = "domain"
+    display_name = "Terraform"
+    home_region = "us-ashburn-1"
+    license_type = "Free"
+}
+
+
