1a3ed3865feb23ed168aee9bd8040fac00427bfa
NacteInd <128390991+NacteInd@users.noreply.github.com>
2023-03-28 14:44:41 -0400

    Update Domain.tf

--- Domain.tf
+++ Domain.tf
@@ -4,6 +4,11 @@ resource "oci_identity_domain" "test_domain" {
     display_name = "Terraform"
     home_region = "us-ashburn-1"
     license_type = "oracle-apps-premium"
+    admin_email = "sysadmin@test.com"
+    admin_first_name = "SysAd"
+    admin_last_name = "Admin"
+    admin_user_name = "Sysadmin"
+    is_notification_bypassed = false
 }
 
 
